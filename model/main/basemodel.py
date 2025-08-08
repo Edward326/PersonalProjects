587d06b6c1b469c06987dc07aeefc6284e79122e
eduard.vesea <eduard.vesea@student.upt.ro>
2025-08-08 18:18:48 +0300

    first_commit

---
+++ model/main/basemodel.py
@@ -0,0 +1,8 @@
+#arhitectura de baza:
+#YOLOv8+LightCap
+
+#YoloV8:
+#rescris arhit astfel incat sa indeplienasca rolul de foreground detector(bb with scales, not conf,not classes scores)
+
+#LightCap:
+#rescris arhitectura astfel uicnat sa poate folosii YoloV8 ca detector de foreground  
